9f2184140e620c2041350f429792104f89487fdb
Richard Clarke <freshnewrich@gmail.com>
2022-05-14 11:58:04 -0700

    links array, map function

--- src/pages/Currencies.jsx
+++ src/pages/Currencies.jsx
@@ -1,3 +1,24 @@
-export default function Currencies (props) {
-    return <h1>This is the Currencies Component</h1>;
+import {Link} from  "react-router-dom"
+
+export default function Currencies(props) {
+    const currencies = [
+        { name: "Bitcoin", symbol: "BTCUSD" },
+        { name: "Litecoin", symbol: "LTCUSD" },
+        { name: "Ethereum", symbol: "ETHUSD" },
+        { name: "Ethereum Classic", symbol: "ETCUSD" },
+        { name: "Stellar Lumens", symbol: "XLMUSD" },
+        { name: "Dash", symbol: "DASHUSD" },
+        { name: "Ripple", symbol: "XRPUSD" },
+        { name: "Zcash", symbol: "ZECUSD" },
+    ]
+    return (
+        <div className="currencies">
+            {currencies.map(({ name, symbol }, index) => {
+                return (<Link key={index} to={`/price/${symbol}`}>
+                    <h2>{name}</h2>
+                </Link>)
+            } )
+            }
+        </div>
+    );
 }
